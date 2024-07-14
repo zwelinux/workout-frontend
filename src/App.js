a26717629e4b38f6a1330acbe6c4f90562eb0a6a
zwelinux <zwelinux@gmail.com>
2024-07-14 13:58:59 +0630

    new commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        HELLO FROM THE OTHER SIDE
-      </header>
-    </div>
-  );
+
+const App = () => {
+  return(
+    <>
+      <Header />
+      <p>App.js</p>
+    </>
+  )
 }
 
-export default App;
+export default App 
